0c1e75c469ad41146ba76fe1d9827533ab3f9615
Sebastian Duque <sebastian.duque@payulatam.com>
2017-04-03 08:00:13 -0500

    03/04/2017
    03/04/2017

--- js/getValues.js
+++ js/getValues.js
@@ -1,55 +1,54 @@
 /**
  * @author Sebastián Duque <sebastian.duque@payulatam.com>
  */
-
 $(document).ready(function() {
-    // Function which store in a variable the GET url and split the values of each field.
-    function load(variable) {
-        var query = window.location.search.substring(1);
-        var vars = query.split("&");
-        for (var i = 0; i < vars.length; i++) {
-            var pair = vars[i].split("=");
-            if (pair[0] == variable) {
-                return pair[1];
-            }
-        }
-        return (false);
-    };
 
-    // Evento el cual ejecuta la funcíon load al cargar el documento.
-    window.onload = load;
+    var query = location.search.substring(1);
+    var queryFormat = query;
 
-    // Creación y inicializaciòn de variables usando la función Load
-    // Creation and initialization of variables using the function Load
-    var merchantId = load("merchantId");
-    var referenceCode = load("referenceCode");
-    var transactionId = load("transactionId");
+    // Evento el cual ejecuta la funcíon queryVariable al cargar la pagina.
+    // Event which executes the function queryVariable when the page load.
+    window.onload = queryVariable;
+    window.onload = tableLog;
+
+    // Creación y inicializaciòn de variables usando la función queryVariable
+    // Creation and initialization of variables using the function queryVariable
+    var merchantId = queryVariable("merchantId");
+    var referenceCodeRequest = decodeURIComponent(queryVariable("referenceCode"));
+    console.log(referenceCodeRequest);
+    var referenceCode = referenceCodeRequest.replace('%3A', ':');
+    referenceCode = referenceCodeRequest.replace('+', ' ');
+    var transactionId = queryVariable("transactionId");
     var processingDate = moment().format('L, h:mm:ss a');
-    var cus = load("cus");
-    var pseBank = decodeURIComponent(load("pseBank"));
-    var amount = load("TX_VALUE");
-    var currency = load("currency");
-    var description = load("description");
-    var ipAddress = load("pseReference1");
-    var transactionState = load("transactionState");
-    var polResponseCode = load("polResponseCode");
-    var lapPaymentMethodType = load("lapPaymentMethodType");
-    var lapPaymentMethod = load("lapPaymentMethod");
+    var cus = queryVariable("cus");
+    var pseBank = decodeURIComponent(queryVariable("pseBank"));
+    var amount = queryVariable("TX_VALUE");
+    var amountTrunc = truncate(amount, 1);
+    var currency = queryVariable("currency");
+    var description = queryVariable("description");
+    var ipAddress = queryVariable("pseReference1");
+    var transactionState = queryVariable("transactionState");
+    var polResponseCode = queryVariable("polResponseCode");
+    var lapPaymentMethodType = queryVariable("lapPaymentMethodType");
+    var lapPaymentMethod = queryVariable("lapPaymentMethod");
+    var signatureRequest = queryVariable("signature");
 
-    // Muestra en la consola el los valores de las variables
-    // Show in the console log the values of the variables.
-    console.log(merchantId + "\n");
-    console.log(referenceCode + "\n");
-    console.log(transactionId + "\n");
-    console.log(cus + "\n");
-    console.log(pseBank + "\n");
-    console.log(amount + "\n");
-    console.log(currency + "\n");
-    console.log(description + "\n");
-    console.log(ipAddress + "\n");
-    console.log(state + "\n");
-    console.log(lapPaymentMethodType + "\n");
-    console.log(lapPaymentMethod + "\n");
+    // Comparación de firmas
+    // Comparation of signatures
+    var signatureGenerated = signatureGenerator(referenceCode, amountTrunc, currency, transactionState);
+    console.log("Generador" + " " + signatureGenerated);
+    console.log("Request" + " " + signatureRequest);
+
+    if (signatureRequest != signatureGenerated) {
+        $("#responseTable").hide();
+        $("#buttons").hide();
+        $("#responseDiv").append("<h1 class='text-center'>LA FIRMA DIGITAL NO ES VÁLIDA</h1>" + "<br>");
+
+
+    } else {
+        $("#responseTable").show();
+    }
+
 
     // Controlador de posibles valores de estados de la transacción
     // Controller of possible values of states of transaction
@@ -72,18 +71,16 @@ $(document).ready(function() {
     }
 
 
-
     // Asignación de valores en campos de la pagina de respuesta (PaginaRespuesta.html).
     // Assignation of values in fields of the response page (PaginaRespuesta.html).
     $('#processingDate').html($.now());
     $('#state').html(state);
     $('#referenceCode').html(referenceCode);
     $('#processingDate').html(processingDate);
-    $('#amount').html(amount + " " + load("currency"));
-    $('#referencePol').html(load("reference_pol"));
+    $('#amount').html(amount + " " + queryVariable("currency"));
+    $('#referencePol').html(queryVariable("reference_pol"));
     $('#description').html(description);
     $('#paymentMethod').html(lapPaymentMethod);
-
     if (lapPaymentMethod == "PSE") {
         $("#trCus").show();
         $("#trPseBank").show();
@@ -93,28 +90,66 @@ $(document).ready(function() {
         $('#ipAddress').html(ipAddress);
     } else if (lapPaymentMethodType == "CREDIT_CARD" && transactionState == 4) {
         $("#trTrazabilityCode").show();
-        $('#trazabilityCode').html(load("trazabilityCode"));
+        $('#trazabilityCode').html(queryVariable("trazabilityCode"));
     } else {
         console.log("No se habilitan campos extra");
     }
-    window.onload = tableLog;
-    // asignación de valores sobre la tabla de logs.
-    // Asignation of values to the log table  
+
+    // Asignation of class according to the state
+    // Asignación de clase de acuerdo al estado
+    $('tr:has(td:contains("aprobada"))').addClass('success');
+    $('tr:has(td:contains("rechazada"))').addClass('danger');
+    $('tr:has(td:contains("fallida"))').addClass('danger');
+    $('tr:has(td:contains("pendiente"))').addClass('warning');
+
+    // Asignación de valores sobre la tabla de logs.
+    // Asignation of values to the log table
     function tableLog() {
-        var query, parms, i, pos, key, val;
-        query = location.search.substring(1);
-        parms = query.split('&');
-        for (i = parms.length - 1; i >= 0; i--) {
-            pos = parms[i].indexOf('=');
+        var parms = query.split('&');
+        console.log("LOG" + "\n");
+        for (var i = parms.length - 1; i >= 0; i--) {
+            var pos = parms[i].indexOf('=');
             if (pos > 0) {
-                key = parms[i].substring(0, pos);
-                val = parms[i].substring(pos + 1);
+                var key = parms[i].substring(0, pos);
+                var val = parms[i].substring(pos + 1);
+
+                // Muestra en la consola el los valores de las variables
+                // Show in the console log the values of the variables.
+                console.log(key + ": " + val + "\n");
+
                 if (val != "") {
-                    $("#tableLog").append("<tr><td id='variable'>" + "<i class='glyphicon glyphicon-check'>" + " " + key + "</td>" + "<td id='value'>" + val + "</td></tr>");
+                    $("#tableLog").append(" " + "<tr><td id='variable'>" + "<i class='glyphicon glyphicon-check'>" + " " + key + "</td>" + "<td id='value'>" + val + "</td></tr>");
                 } else {
-                    $("#tableLog").append("<tr><td id='variable'> " + "<i class='glyphicon glyphicon-unchecked'>" + " " + key + "</td>" + "<td id='value'>" + val + "</td></tr>");
+                    $("#tableLog").append(" " + "<tr><td id='variable'> " + "<i class='glyphicon glyphicon-unchecked'>" + " " + key + "</td>" + "<td id='value'>" + val + "</td></tr>");
                 }
             }
         }
     };
+
+    // Función para truncar los decimales de la variable amount
+    // Function which truncate the decimal of amount variable
+    function truncate(num, places) {
+        return Math.trunc(num * Math.pow(10, places)) / Math.pow(10, places);
+    }
+
+    // Función que recibe el nombre de la variable y la busca en todos los parametros de la URL
+    // Function which receives the name of the variable and searches it in the entire parameters from the URL
+    function queryVariable(variable) {
+        var vars = query.split("&");
+        for (var i = 0; i < vars.length; i++) {
+            var pair = vars[i].split("=");
+            if (pair[0] == variable) {
+                return pair[1];
+            }
+        }
+        return (false);
+    };
+
+    // Ocultar los datos GET de la URL
+    // Hide the GET data from the URL
+    function formatURL(queryFormat) {
+        queryFormat = "d:/sebastian.duque/Documents/GitHub/Pagina-de-respuesta/PaginaRespuesta.html";
+        history.pushState({}, 'The title', queryFormat);
+    };
+    formatURL(query);
 });
--- js/signatureGenerator.js
+++ js/signatureGenerator.js
@@ -1,26 +1,19 @@
-$(document).ready(function() {
-    $("#post").click(function() {
-		
-		// Credentials
+    function signatureGenerator(referenceCodeRequest, amountRequest, currencyRequest, transactionStateRequest) {
+        // Credentials
         var apiKey = "4Vj8eK4rloUd272L48hsrarnUA";
         var merchanId = "508029";
-        var accountId_ = "512321";
-		
-		// Get information of the form
-        var amount = document.getElementById('amount').value;
-        var currency = document.getElementById('currency').value;
-        var time_ = $.now();
-        document.getElementById('referenceCode').value = time_;
-		
-		// Signature MD5 generator
-        var signature = String(CryptoJS.MD5(apiKey + "~" + merchanId + "~" + time_ + "~" + amount + "~" + currency));
-        document.getElementById('signature').value = signature;
-		
-		// Console log of the references and signature values
-        console.log(time_);
+        var accountId = "512321";
+
+        // Get information of the form
+        var reference = referenceCodeRequest;
+        var amount = amountRequest;
+        var currency = currencyRequest;
+        var transactionState = transactionStateRequest;
+
+        // Signature MD5 generator
+        var signature = String(CryptoJS.MD5(apiKey + "~" + merchanId + "~" + reference + "~" + amount + "~" + currency + "~" + transactionState));
+        return signature;
+
+        // Console log signature value
         console.log(signature);
-		
-		// Action for post
-        document.getElementById('form').submit();
-    });
-});
+    };
